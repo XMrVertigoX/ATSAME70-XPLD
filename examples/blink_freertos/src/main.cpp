039d383cb4b4db933bab83ba407a4943115930c8
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2016-06-16 17:07:20 +0200

    different blink pattern

--- examples/blink_freertos/src/main.cpp
+++ examples/blink_freertos/src/main.cpp
@@ -9,10 +9,10 @@ void task(void *user) {
     ioport_set_pin_dir(LED0_GPIO, IOPORT_DIR_OUTPUT);
 
     for (;;) {
-        ioport_set_pin_level(LED0_GPIO, IOPORT_PIN_LEVEL_HIGH);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
         ioport_set_pin_level(LED0_GPIO, IOPORT_PIN_LEVEL_LOW);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+        ioport_set_pin_level(LED0_GPIO, IOPORT_PIN_LEVEL_HIGH);
+        vTaskDelay(950 / portTICK_PERIOD_MS);
     }
 }
 
