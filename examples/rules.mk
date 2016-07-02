fed0a591ef832d03155ff3963b36fd571d3492c1
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2016-07-02 23:23:58 +0200

    minimize sdk

--- examples/rules.mk
+++ examples/rules.mk
@@ -1,5 +1,6 @@
 # ----- Files ------------------------------------------------------------------
 
+BINARY     = $(OUTPUT_DIR)/$(PROJECT_NAME).bin
 EXECUTABLE = $(OUTPUT_DIR)/$(PROJECT_NAME).elf
 MAPFILE    = $(OUTPUT_DIR)/$(PROJECT_NAME).map
 
@@ -23,7 +24,7 @@ SORTED_OBJECT_FILES = $(addprefix $(OBJECT_DIR),$(addsuffix .o,$(basename $(SORT
 
 .PHONY: all clean download
 
-all: $(EXECUTABLE)
+all: $(EXECUTABLE) $(BINARY)
 	@echo # New line for better reading
 	@$(SIZE) $<
 	@echo # Another new line for even better reading
@@ -37,6 +38,9 @@ download: $(EXECUTABLE)
 
 $(EXECUTABLE): $(SORTED_OBJECT_FILES)
 	@echo [ LNK ] $(notdir $@) & $(MKDIR) $(dir $@) & $(CC) $(GCCFLAGS) $(LDFLAGS) $^ $(LIBFLAGS) -o $@
+
+$(BINARY): $(EXECUTABLE)
+	@echo [ CPY ] $(notdir $@) & $(MKDIR) $(dir $@) & $(OBJCOPY) -O binary $< $@
 
 $(OBJECT_DIR)/%.o: /%.c
 	@echo [ CMP ] $(notdir $@) & $(MKDIR) $(dir $@) & $(CC) $(GCCFLAGS) $(CFLAGS) $(CPPFLAGS) -c -o $@ $<
