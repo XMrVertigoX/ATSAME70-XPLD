d8085f79a88aef2c6f7985ee81d77544e6360515
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2016-07-30 19:32:39 +0200

    improve logging
    switch from defines to constants

--- examples/nrf24l01/src/lib/util/logging.cpp
+++ examples/nrf24l01/src/lib/util/logging.cpp
@@ -7,31 +7,31 @@
 
 #include "logging.hpp"
 
-#define MAX_LOG_LENGTH 256
-#define REPLACEMENT_CHARACTER '_'
+static const size_t numBytes = 256;
+static const char replacementCharacter = '_';
 
-static void putVisibleChararcterRepresentation(char character) {
+static char bytes[numBytes];
+
+static char getVisibleChararcterRepresentation(char character) {
     if (isprint(character)) {
-        putchar(character);
+        return character;
     } else {
-        putchar(REPLACEMENT_CHARACTER);
+        return replacementCharacter;
     }
 }
 
 void printVisible(const char *format, ...) {
     portENTER_CRITICAL();
 
-    static char bytes[MAX_LOG_LENGTH];
-
-    size_t numBytes;
+    size_t length;
     va_list args;
 
     va_start(args, format);
-    numBytes = vsnprintf(bytes, sizeof(bytes), format, args);
+    length = vsnprintf(bytes, sizeof(bytes), format, args);
     va_end(args);
 
-    for (size_t i = 0; i < numBytes; i++) {
-        putVisibleChararcterRepresentation(bytes[i]);
+    for (size_t i = 0; i < length; i++) {
+        putchar(getVisibleChararcterRepresentation(bytes[i]));
     }
 
     putchar('\n');
--- examples/nrf24l01/src/lib/util/logging.hpp
+++ examples/nrf24l01/src/lib/util/logging.hpp
@@ -6,8 +6,8 @@
 
 #define TIMESTAMP "%d.%03d"
 #define FUNCTION "%s"
-#define SECONDS xTaskGetTickCount() * portTICK_PERIOD_MS / 1000
-#define MILLISECONDS xTaskGetTickCount() * portTICK_PERIOD_MS % 1000
+#define SECONDS (xTaskGetTickCount() * portTICK_PERIOD_MS / 1000)
+#define MILLISECONDS (xTaskGetTickCount() * portTICK_PERIOD_MS % 1000)
 
 #ifndef NDEBUG
 #define LOG(MESSAGE, ...)                                                    \
