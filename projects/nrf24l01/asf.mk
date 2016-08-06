7bd1521badb820ddf92e5cb1533370a80c20379e
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2016-08-06 16:25:20 +0200

    repair

--- projects/nrf24l01/asf.mk
+++ projects/nrf24l01/asf.mk
@@ -14,14 +14,13 @@ SYMBOLS += ARM_MATH_CM7=true
 # SYMBOLS += printf=iprintf
 # SYMBOLS += scanf=iscanf
 
-# ----- ASF ------------------------------------------------------------------
+# ----- Source files ---------------------------------------------------------
 
 INCLUDE_DIRS += $(COMMON)/boards
 INCLUDE_DIRS += $(COMMON)/services/clock
 INCLUDE_DIRS += $(COMMON)/services/ioport
 INCLUDE_DIRS += $(COMMON)/utils
 INCLUDE_DIRS += $(SAM)/boards
-INCLUDE_DIRS += $(SAM)/drivers/mpu
 INCLUDE_DIRS += $(SAM)/drivers/pio
 INCLUDE_DIRS += $(SAM)/drivers/pmc
 INCLUDE_DIRS += $(SAM)/drivers/spi
@@ -36,9 +35,8 @@ INCLUDE_DIRS += $(THIRDPARTY)/CMSIS/Include
 SOURCE_FILES += $(COMMON)/services/clock/same70/sysclk.c
 SOURCE_FILES += $(COMMON)/utils/interrupt/interrupt_sam_nvic.c
 SOURCE_FILES += $(SAM)/boards/same70_xplained/init.c
-#SOURCE_FILES += $(SAM)/drivers/mpu/mpu.c
-#SOURCE_FILES += $(SAM)/drivers/pio/pio.c
-#SOURCE_FILES += $(SAM)/drivers/pio/pio_handler.c
+SOURCE_FILES += $(SAM)/drivers/pio/pio.c
+SOURCE_FILES += $(SAM)/drivers/pio/pio_handler.c
 SOURCE_FILES += $(SAM)/drivers/pmc/pmc.c
 SOURCE_FILES += $(SAM)/drivers/spi/spi.c
 SOURCE_FILES += $(SAM)/utils/cmsis/same70/source/templates/system_same70.c
