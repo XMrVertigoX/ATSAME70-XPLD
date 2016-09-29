f99e525245b40df335c321c815beb779b2084fa4
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2016-09-29 12:55:07 +0200

    Sleep manager intermediate

--- nrf24l01/src/sleepmanager.cpp
+++ nrf24l01/src/sleepmanager.cpp
@@ -9,8 +9,48 @@
 
 void SleepManager::init() {
     sleepmgr_init();
+    // Init counter/timer
+}
+
+uint32_t SleepManager::sleep(TickType_t ticks) {
+    // sleepmgr_enter_sleep();
+    // TODO: Return actual rtos sleep ticks
+    return (0);
+}
+
+TickType_t SleepManager::rtc2rtos(uint32_t ticks) {
+    // return ((ticks * configTICK_RATE_HZ) / _clockFreqency);
+    return (0);
+}
+
+uint32_t SleepManager::rtos2rtc(TickType_t ticks) {
+    // return ((ticks * _clockFreqency) / configTICK_RATE_HZ);
+    return (0);
 }
 
 // ----- FreeRTOS hook function -----------------------------------------------
 
-extern "C" void vPortSuppressTicksAndSleep(TickType_t xExpectedIdleTime) {}
+void vPortSuppressTicksAndSleep(TickType_t expectedSleepTicks) {
+    uint32_t actualSleepTicks = 0;
+
+    SleepManager &sleepManager = SleepManager::getInstance();
+    eSleepModeStatus eSleepStatus = eTaskConfirmSleepModeStatus();
+
+    if (eSleepStatus == eAbortSleep) {
+        return;
+    }
+
+    // TODO: Disable timer/interrupts here
+    taskDISABLE_INTERRUPTS();
+
+    if (eSleepStatus == eStandardSleep) {
+        actualSleepTicks = sleepManager.sleep(expectedSleepTicks);
+    } else {
+        actualSleepTicks = sleepManager.sleep();
+    }
+
+    vTaskStepTick(actualSleepTicks);
+
+    // TODO: Enable timer/interrupts here
+    taskENABLE_INTERRUPTS();
+}
--- nrf24l01/src/sleepmanager.hpp
+++ nrf24l01/src/sleepmanager.hpp
@@ -3,6 +3,9 @@
 
 #include <asf.h>
 
+#include <FreeRTOS.h>
+#include <task.h>
+
 #include <xXx/templates/singleton.hpp>
 
 using namespace xXx;
@@ -12,10 +15,14 @@ class SleepManager : public Singleton<SleepManager> {
 
    public:
     void init();
+    uint32_t sleep(TickType_t ticks = 0);
 
    private:
     SleepManager() = default;
     ~SleepManager() = default;
+
+    TickType_t rtc2rtos(uint32_t ticks);
+    uint32_t rtos2rtc(TickType_t ticks);
 };
 
 #endif /* SLEEPMANAGER_HPP_ */
