ed2b40cc9e11b1026812e93786e239d021480851
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-01-06 20:17:49 +0100

    Updated submodule libs/xXx

--- nrf24l01/src/radiotask.cpp
+++ nrf24l01/src/radiotask.cpp
@@ -14,16 +14,15 @@ RadioTask::RadioTask(nRF24L01 &rf24) : ArduinoTask(1024, 1), _rf24(rf24) {}
 RadioTask::~RadioTask() {}
 
 void RadioTask::setup() {
-    uint8_t buffer[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vTaskDelay(100 / portTICK_PERIOD_MS);
 
-    _rf24.begin();
+    uint8_t buffer[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    _rf24.init();
 
-    vTaskDelay(1 / portTICK_PERIOD_MS);
+    _rf24.openWritingPipe(0xE0E1E2E3E4);
+    //    _rf24.openReadingPipe(1, 0xE0E1E2E3E4);
 
-    LOG("%d", _rf24.getPALevel());
-    _rf24.setPALevel(RF24_PA_18dBm);
-    LOG("%d", _rf24.getPALevel());
-    _rf24.openWritingPipe(0);
     _rf24.write(buffer, sizeof(buffer));
 }
 
