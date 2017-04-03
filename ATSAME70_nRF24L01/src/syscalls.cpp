ddfc7a5d1fde6d49e09de6c0f89f2faabaa63038
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-04-03 21:47:04 +0200

    Simplify syscalls, share rtt libraries

--- ATSAME70_nRF24L01/src/syscalls.cpp
+++ ATSAME70_nRF24L01/src/syscalls.cpp
@@ -3,13 +3,9 @@
 #include <SEGGER_RTT.h>
 
 extern "C" int _write(int f, char *bytes, int numBytes) {
-    SEGGER_RTT_Write(0, bytes, numBytes);
-
-    return (0);
+    return (SEGGER_RTT_Write(0, bytes, numBytes));
 }
 
 extern "C" int _write_r(struct _reent *r, int f, const void *bytes, size_t numBytes) {
-    SEGGER_RTT_Write(0, bytes, numBytes);
-
-    return (0);
+    return (SEGGER_RTT_Write(0, bytes, numBytes));
 }
