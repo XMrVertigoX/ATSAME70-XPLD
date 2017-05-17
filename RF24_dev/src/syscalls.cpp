5e467157d8ec1f7ecc73c6429746fb2f6dd958f3
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-05-17 12:40:37 +0200

    Updated submodule libs/xXx

--- RF24_dev/src/syscalls.cpp
+++ RF24_dev/src/syscalls.cpp
@@ -3,9 +3,14 @@
 #include <SEGGER_RTT.h>
 
 extern "C" int _write(int f, char *bytes, size_t numBytes) {
+    (void)f;
+
     return (SEGGER_RTT_Write(0, bytes, numBytes));
 }
 
 extern "C" int _write_r(struct _reent *r, int f, const void *bytes, size_t numBytes) {
+    (void)r;
+    (void)f;
+
     return (SEGGER_RTT_Write(0, bytes, numBytes));
 }
