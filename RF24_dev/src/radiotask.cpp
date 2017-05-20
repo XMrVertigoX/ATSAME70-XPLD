c7adbc1e6d9f816d2a3b0b691b83b853d8bf59ce
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-05-20 14:27:07 +0200

    Typo

--- RF24_dev/src/radiotask.cpp
+++ RF24_dev/src/radiotask.cpp
@@ -3,17 +3,17 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
+#include <xXx/components/wireless/rf24/rf24.hpp>
 #include <xXx/os/simpletask.hpp>
 #include <xXx/templates/queue.hpp>
 #include <xXx/utils/logging.hpp>
 
 #include "radiotask.hpp"
-#include <xXx/components/wireless/rf24/rf24.hpp>
 
 using namespace xXx;
 
-static const RF24_Address_t address_0 = 0xE7E7E7E7E7;
-static const RF24_Channel_t channel   = 2;
+static const RF24_Address_t address = 0xE7E7E7E7E7;
+static const RF24_Channel_t channel = 2;
 
 RadioTask::RadioTask(RF24& receiver)
     : receiver(receiver), led(Gpio(LED_0_PIN)), rxQueue(Queue<RF24_DataPackage_t>(3)) {}
@@ -27,7 +27,7 @@ void RadioTask::setup() {
 
     receiver.setup();
 
-    status = receiver.setRxAddress(0, address_0);
+    status = receiver.setRxAddress(0, address);
     status = receiver.setDataRate(RF24_DataRate::DR_2MBPS);
     status = receiver.setCrcConfig(RF24_CRCConfig::CRC_2Bytes);
     status = receiver.setChannel(channel);
