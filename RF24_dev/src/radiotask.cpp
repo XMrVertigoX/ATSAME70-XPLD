6e2481534a1806aea93473d7a548984a6d6f57a3
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2017-06-04 02:54:46 +0200

    Updated submodule libs/xXx

--- RF24_dev/src/radiotask.cpp
+++ RF24_dev/src/radiotask.cpp
@@ -14,6 +14,11 @@ using namespace xXx;
 
 static const RF24_Address_t address_p0 = 0xE7E7E7E7E7;
 static const RF24_Address_t address_p1 = 0xC2C2C2C2C2;
+static const RF24_Address_t address_p2 = 0xC2C2C2C2C3;
+static const RF24_Address_t address_p3 = 0xC2C2C2C2C4;
+static const RF24_Address_t address_p4 = 0xC2C2C2C2C5;
+static const RF24_Address_t address_p5 = 0xC2C2C2C2C6;
+
 static const RF24_Channel_t channel = 2;
 
 RadioTask::RadioTask(RF24& receiver)
@@ -28,13 +33,25 @@ void RadioTask::setup() {
 
     receiver.setup();
 
-    status = receiver.setRxAddress(0, address_p1);
-    status = receiver.setRxAddress(1, address_p0);
-    status = receiver.setDataRate(RF24_DataRate::DR_2MBPS);
-    status = receiver.setCrcConfig(RF24_CRCConfig::CRC_2Bytes);
     status = receiver.setChannel(channel);
-    status = receiver.configureRxDataPipe(1, &rxQueue);
+    assert(status == RF24_Status::Success);
 
+    status = receiver.setCrcConfig(RF24_CRCConfig::CRC_2Bytes);
+    assert(status == RF24_Status::Success);
+
+    status = receiver.setDataRate(RF24_DataRate::DR_2MBPS);
+    assert(status == RF24_Status::Success);
+
+    status = receiver.setRxAddress(0, address_p0);
+    assert(status == RF24_Status::Success);
+
+    status = receiver.setRxAddress(1, address_p1);
+    assert(status == RF24_Status::Success);
+
+    status = receiver.setRxAddress(2, address_p2);
+    assert(status == RF24_Status::Success);
+
+    status = receiver.configureRxDataPipe(0, &rxQueue);
     assert(status == RF24_Status::Success);
 
     receiver.enterRxMode();
